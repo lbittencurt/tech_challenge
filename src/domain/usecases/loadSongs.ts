63a99b9e2aa7944852738530fc7eae1aaa000af4
Lucas Bittencurt <bittencurt.lucas@outlook.com>
2021-01-17 15:41:28 -0300

    feat: create domain layer with loadSongs use case

---
+++ src/domain/usecases/loadSongs.ts
@@ -0,0 +1,28 @@
+export interface LoadSongs {
+  loadAll: () => Promise<LoadSongs.Model[]>
+}
+
+export namespace LoadSongs {
+  export type Model = {
+    song: string
+    artist: string
+    songReleaseDate: string
+    playCount: number
+    metricA: number
+    metricB: number
+    metricC: number
+    metricD: number
+    metricE: number
+    metricF: number
+    metricG: number
+    metricH: number
+    metricI: number
+    metricJ: number
+    metricK: number
+    metricL: number
+    metricM: number
+    metricN: number
+    metricO: number
+    metricP: number
+  }
+}
