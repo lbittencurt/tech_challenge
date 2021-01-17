8a8f237f4092db4498a20c5462c4f0407d60ea80
Lucas Bittencurt <bittencurt.lucas@outlook.com>
2021-01-17 16:48:50 -0300

    feat: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
 import Main from './main/main'
 import reportWebVitals from './reportWebVitals'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
+import GlobalStyle from './styles/global'
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Main />
+      <GlobalStyle />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
